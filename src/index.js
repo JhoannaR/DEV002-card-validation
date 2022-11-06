39bac05870123f3fc6a42fd80bcb5a39e7d741e9
Jhoanna Rosa Mera Dávila <jhoannarosameradavila@gmail.com>
2022-11-05 21:27:23 -0500

    hasta ahí ya funciona validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import validator from './validator.js';
+const inputNumero = document.getElementById("inputNumber")
+//const inputUsuario = document.getElementById("user")
 
-console.log(validator);
+const verificara = document.getElementById('botonValidar')
+const ocultara = document.getElementById('botonValidar')
+
+
+verificara.addEventListener('click', function () {
+    var valid = validator.isValid(inputNumero.value);
+    if (valid === true)
+        console.log("valida");
+    else
+        console.log("invalida");
+});
+
+ocultara.addEventListener('click', function () {
+    var hidden = validator.maskify(inputNumero.value);
+        console.log(hidden);
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//formulario -> input -> usuario
+//cmo puedo obtener el valor de un input?¨
+//cuando evaluar la tarjeta? (usuario decida validarla? - button)
+
+//prevenir el reload (recarga de pagina) evento submit formulario
+
+//como seleccionar un elemento de html en javascript
+//document.getElement
+//elemento seleccionado agregar un evento ()
+
+//esta funcion ya hace la logica de validar
+//espera recibir un numero y devuelve ... un boolean (true,false)
+
+
+
