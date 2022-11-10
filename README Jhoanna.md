7576014d6102342cab0bca80dc41119d0812fd91
Jhoanna Rosa Mera Dávila <jhoannarosameradavila@gmail.com>
2022-11-10 12:15:34 -0500

    ya se ve en mi modal el resultado de la funcion Maskify

--- README Jhoanna.md
+++ README Jhoanna.md
@@ -5,7 +5,7 @@
 
 En el `README.md`, cuéntanos cómo pensaste en los usuarios y cuál fue tu proceso
 para definir el producto final a nivel de experiencia y de interfaz:
-
+git
 Debido a que hoy en día vivimos en un mundo globalizado donde cada vez son mas recurrentes las compras en línea por diferentes motivos tales como: el estilo de vida ajetreado, la pandemia del covid 19, la inseguridad ciudadana, entre otros factores; pensé en incorporar este código para que forme parte de un sitio web de compras de ropa para mujeres jóvenes de alguna galería del emporio textil de Gamarra de Lima Perú que verían incrementadas sustancialmene sus ventas de contar con su propia aplicación web.
 
 * Quiénes son los principales usuarios de producto: Mujeres jóvenes que cuenten con alguna tarjeta de débito o crédito.
@@ -13,7 +13,7 @@ Debido a que hoy en día vivimos en un mundo globalizado donde cada vez son mas 
 * Cómo crees que el producto que estás creando está resolviendo sus problemas.
 
 ### Prototipo Final
-[Prototipo en Figma de Credit Card Validator](http://https://www.figma.com/file/dFAW4t9ccGUQ0MMZjNEu7E/PRIMER-PROTOTIPO-1Proyecto-Jhoanna?node-id=0%3A1 "Prototipo en Figma de Credit Card Validator")
+[Prototipo en Figma de Credit Card Validator](https://www.figma.com/file/dFAW4t9ccGUQ0MMZjNEu7E/PRIMER-PROTOTIPO-1Proyecto-Jhoanna?node-id=0%3A1 "Prototipo en Figma de Credit Card Validator")
 
 
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import validator from './validator.js';
 const inputNumero = document.getElementById("inputNumber")
 const inputUsuario = document.getElementById("user")
 
-const modalAdd= document.querySelector('#modalAdd');
-const modalAddValid= document.querySelector('#modalAddValid');
+const modalAdd = document.querySelector('#modalAdd');
+const modalAddValid = document.querySelector('#modalAddValid');
 
 const botonCloseModal = document.querySelector('#botonCloseModal')
 const botonCloseModalValid = document.querySelector('#botonCloseModalValid')
@@ -14,51 +14,48 @@ const prueba = document.getElementById('prueba')
 const pruebaValid = document.getElementById('pruebaValid')
 
 
-
-const openModal = () =>{
-    modalAdd.style.display='flex';
-    prueba.textContent  = "hola "+inputUsuario.value+ ", el número de tarjeta que ingresaste no existe o te equivocaste en digitar. Deseas probar de nuevo o deseas cancelar la compra"
-
-    //texto en html <p> 
-    //p.textContent = "#####3412"
-    //p.innerHtml
+const openModal = () => {
+  modalAdd.style.display = 'flex';
+  prueba.textContent = "hola " + inputUsuario.value + ", el número de tarjeta que ingresaste: "+validator.maskify(inputNumero.value)+" no existe o te equivocaste en digitar. Deseas probar de nuevo o deseas cancelar la compra";
+  //texto en html <p> 
+  //p.textContent = "#####3412"
+  //p.innerHtml
 }
-const openModalValido = () =>{
-  modalAddValid.style.display='flex';
-  pruebaValid.textContent  = "Muy bien "+inputUsuario.value+ " ahora nos comunicaremos con el banco para realizar tu pago"
+const openModalValido = () => {
+  modalAddValid.style.display = 'flex';
+  pruebaValid.textContent = "Muy bien " + inputUsuario.value + " ahora nos comunicaremos con el banco para realizar tu pago"+validator.maskify(inputNumero.value);
 }
 
-function closeModal () {
-  modalAdd.style.display='none';
+function closeModal() {
+  modalAdd.style.display = 'none';
 }
-function closeModalValid () {
-  modalAddValid.style.display='none';
+function closeModalValid() {
+  modalAddValid.style.display = 'none';
 }
-  
-  
-botonCloseModal.addEventListener('click',closeModal)
-botonCloseModalValid.addEventListener('click',closeModalValid)
+
+
+botonCloseModal.addEventListener('click', closeModal)
+botonCloseModalValid.addEventListener('click', closeModalValid)
 
 verificara.addEventListener('click', function () {
-    var valid = validator.isValid(inputNumero.value);
-    if (valid === true){
-        
-        console.log("valida");
-        openModalValido()
-  
-    }
-        
-    else{
-      console.log("invalida");
-      openModal()
-    }
+  var valid = validator.isValid(inputNumero.value);
+  if (valid === true) {
+
+    console.log("valida");
+    openModalValido()
+
+  }
+
+  else {
+    console.log("invalida");
+    openModal()
+  }
 });
 
 ocultara.addEventListener('click', function () {
-    var hidden = validator.maskify(inputNumero.value);
-        console.log(hidden);
+ var hidden = validator.maskify(inputNumero.value);
+  console.log(hidden);
 
-    
 });
 
 
